1c85752ba0df9549afbb61cd865cebf902b09cd7
mugambiDaktari <wycliffmutethia@gmail.com>
2025-02-01 12:40:08 +0300

    Magical Database for Your Dream Online Bookstore Adventure!

---
+++ MySQLServer.py
@@ -0,0 +1,40 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+USE alx_book_store;
+
+CREATE TABLE Books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+);
+
+CREATE TABLE Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE Order_Details(
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT, 
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    book_id INT,
+    FOREIGN KEY(book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);
