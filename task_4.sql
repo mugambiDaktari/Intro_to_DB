5e2bcf9c141f07a2f82812158a158c4ff63988b9
mugambiDaktari <wycliffmutethia@gmail.com>
2025-02-01 16:26:50 +0300

    showing Book table description ai assisted

--- task_4.sql
+++ task_4.sql
@@ -1,3 +1,20 @@
 USE alx_book_store;
 
-SELECT * FROM Books;
+
+
+USE alx_book_store;
+
+
+SHOW TABLES;
+
+-- Show the description of the 'Books' table by querying INFORMATION_SCHEMA.COLUMNS
+SELECT 
+    COLUMN_NAME, 
+    COLUMN_TYPE, 
+    IS_NULLABLE
+FROM 
+    INFORMATION_SCHEMA.COLUMNS 
+WHERE 
+    TABLE_SCHEMA = 'alx_book_store' 
+    AND TABLE_NAME = 'Books';
+
