ef0814e68dd067e3ce47f4fef2c9b0641f4a3933
mugambiDaktari <wycliffmutethia@gmail.com>
2025-02-01 15:42:49 +0300

    first trial with my_cursor

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,4 +1,63 @@
-CREATE DATABASE IF NOT EXISTS alx_book_store;
+import mysql.connector
+
+# Database connection details (replace with your own)
+try:
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Cliff4950*",
+        database="alx_book_store"
+    )
+    print("Connection successful!")
+except mysql.connector.Error as err:
+    print(f"Error: {err}")
+    exit(1)
+mycursor = mydb.cursor()
+
+# Create a table named `customers` (if it doesn't exist)
+mycursor.execute(
+""" CREATE TABLE Books(
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130),
+    author_id INT,
+    FOREIGN KEY(author_id) REFERENCES Authors(author_id),
+    price DOUBLE,
+    publication_date DATE
+); 
+
+CREATE TABLE Authors(
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215)
+);
+
+CREATE TABLE Customers(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215),
+    email VARCHAR(215),
+    address TEXT
+);
+
+CREATE TABLE Orders(
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES Customers(customer_id),
+    order_date DATE
+);
+
+CREATE TABLE Order_Details(
+    orderdetail_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT, 
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    book_id INT,
+    FOREIGN KEY(book_id) REFERENCES Books(book_id),
+    quantity DOUBLE
+);"""
+)
+
+
+""" print("Table created successfully!") """
+
+""" CREATE DATABASE IF NOT EXISTS alx_book_store;
 
 USE alx_book_store;
 
@@ -37,4 +96,4 @@ CREATE TABLE Order_Details(
     book_id INT,
     FOREIGN KEY(book_id) REFERENCES Books(book_id),
     quantity DOUBLE
-);
+); """
--- trial.sql
+++ trial.sql
@@ -38,3 +38,5 @@ CREATE TABLE Order_Details(
     FOREIGN KEY(book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 );
+
+SHOW DATABASES;
